eddd8b5e60612941b8450f382130631c0a703d18
[IoT1]이수연 <dudgk0216@gmail.com>
2023-06-06 05:42:11 +0900

    로딩바 추가

--- src/components/ProjectVideo.jsx
+++ src/components/ProjectVideo.jsx
@@ -4,6 +4,7 @@ import ValueShare from "../assets/valushare.mp4";
 import SamSamFarm from "../assets/samsamfarm.mp4";
 import HelloJeju from "../assets/헬로우제주.mp4";
 import animationData from "../data/141572-space-boy-developer.json";
+import LoadingData from "../data/97930-loading.json";
 import Lottie from "react-lottie";
 const defaultOptions = {
   loop: true,
@@ -13,7 +14,14 @@ const defaultOptions = {
     preserveAspectRatio: "xMidYMid slice",
   },
 };
-
+const defaultOptions2 = {
+  loop: true,
+  autoplay: true,
+  animationData: LoadingData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 export default function ProjectVideo({
   currentPage,
   isVideoLoaded,
@@ -48,7 +56,10 @@ export default function ProjectVideo({
         )}
       </video>
       {isVideoLoaded === false && (
-        <Lottie options={defaultOptions} height={500} width={500} />
+        <>
+          <Lottie options={defaultOptions} height={500} width={500} />
+          <Lottie options={defaultOptions2} height={100} width={500} />
+        </>
       )}
     </aside>
   );
