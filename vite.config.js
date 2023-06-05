146d835dcbf40532ee9b2883d14d5b8d130b85c0
[IoT1]이수연 <dudgk0216@gmail.com>
2023-06-06 03:33:14 +0900

    배포 에러 재수정

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-  },
-  // resolve: {
-  //   alias: {
-  //     "@react-icons/all-files/si/SiNotion":
-  //       "@react-icons/all-files/es/si/SiNotion.js",
-  //     "@react-icons/all-files/fa/FaBlogger":
-  //       "@react-icons/all-files/fa/FaBlogger.js",
-  //   },
-  // },
 });
