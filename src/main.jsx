04748e02e6ff6bade353506da66419c8665887b4
[IoT1]이수연 <dudgk0216@gmail.com>
2023-06-06 03:45:23 +0900

    배포에러 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
